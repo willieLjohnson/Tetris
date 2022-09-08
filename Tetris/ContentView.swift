1b7ef529fd991458e07dd41f6cc7eafff42e7048
Willie Johnson <liwa.johnson@gmail.com>
2022-09-07 22:06:54 -0400

    Initial Commit

---
+++ Tetris/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tetris
+//
+//  Created by Willie Liwa Johnson on 9/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tetris/TetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetrisApp.swift
+//  Tetris
+//
+//  Created by Willie Liwa Johnson on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
